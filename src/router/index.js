9c86338c92e996dcac334670aba1fafdc50ed729
charliepy <45889586+charliepy@users.noreply.github.com>
2024-02-25 14:21:27 -0500

    Add hash mode

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,27 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import { ROUTES } from '@/router/pages.js';
 
 const routes = [
   {
-    path: '/pickleball-graphs',
+    path: '/',
     name: ROUTES.home,
     component: () => import('@/views/Home.vue'),
     meta: { title: 'Pickleball Graphs' },
   },
   {
-    path: '/pickleball-graphs/pb-effect',
+    path: '/pb-effect',
     name: ROUTES.pbeffect,
     component: () => import('@/views/PickleballEffect.vue'),
     meta: { title: 'Pickleball Effect' },
   },
   {
-    path: '/pickleball-graphs/johnkew-pb',
+    path: '/johnkew-pb',
     name: ROUTES.johnkewpb,
     component: () => import('@/views/JohnkewPickleball.vue'),
     meta: { title: 'Johnkew Pickleball' },
   },
   {
-    path: '/pickleball-graphs/pb-brackets',
+    path: '/pb-brackets',
     name: ROUTES.pbbrackets,
     component: () => import('@/views/PickleballBrackets.vue'),
     meta: { title: 'Pickleball Brackets' },
@@ -35,7 +35,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory('/pickleball-graphs'),
   routes,
 });
 
