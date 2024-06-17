98fb3dab9b21c9b38cea1e8ad6c3736f6edc084e
charliepy <45889586+charliepy@users.noreply.github.com>
2024-06-17 16:25:51 -0400

    v1.0.4 Release
    - Kevlar Stats

--- src/views/MiscStats.vue
+++ src/views/MiscStats.vue
@@ -2,9 +2,16 @@
   <BaseSidebar>
     <article class="mt-4 prose lg:prose-xl max-w-7xl mx-auto">
       <h3>Miscellaneous Stats</h3>
-      <h4>Paddle percentage spin loss from Johnkew Podcast Episode 5</h4>
+      <h4>
+        Kevlar Paddle Shootout Results from Pickleball Effect: The Podcast
+        Episode 41
+      </h4>
+      <img src="/src/assets/stats/Braydon.png" alt="braydon kevlar" />
+      <img src="/src/assets/stats/Kyle.png" alt="kyle kevlar" />
+
+      <h4>Paddle Percentage Spin Loss from Johnkew Podcast Episode 5</h4>
       <img
-        class="lg: w-[70vw]"
+        class="lg:w-[70vw]"
         src="/src/assets/stats/Spin-Loss.png"
         alt="spin loss" />
     </article>
